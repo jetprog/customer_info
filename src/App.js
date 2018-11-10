7142ff45006555a80e656161560341994b6e3ec3
Seth Jetro <jsethjetro@gmail.com>
2018-11-10 15:01:21 -0500

    set container and different item

--- src/App.js
+++ src/App.js
@@ -17,6 +17,88 @@ class App extends Component {
         <div className="App">
           <h1>CUSTOMER INFORMATION</h1>
         </div>
+
+        <div className="basic_info">
+          <h2>Basic Info</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>1</div>
+          <div>2</div>
+        </div>
+
+        <div className="home_info">
+          <h2>Home Info</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        <div className="home_info">
+          <h2>Energy Info</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        <div className="home_info">
+          <h2>Financial Info</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        <div className="basic_info">
+          <h2>Preferred Communication Method</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>1</div>
+          <div>2</div>
+        </div>
+
+        <div className="home_info">
+          <h2>Sales Notes</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        <div className="home_info">
+          <h2>Account Management Notes</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        <div className="home_info">
+          <h2>Project Info</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        <div className="home_info">
+          <h2>Sales Info</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        <div className="home_info">
+          <h2>Load Info</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
+        <div className="home_info">
+          <h2>Uploaded Documents</h2>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+
       </div>
     );
   }
