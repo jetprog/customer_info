455a5cc92b6af90797a806410e80a9b2ddd2bf57
Seth Jetro <jsethjetro@gmail.com>
2018-11-10 14:27:36 -0500

    starting implementation

--- src/App.js
+++ src/App.js
@@ -3,23 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+
+        <div className="App">
+          <h1>CUSTOMER INFORMATION</h1>
+        </div>
       </div>
     );
   }
